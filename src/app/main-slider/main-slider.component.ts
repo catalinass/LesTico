023bd95fd780ea62d605bb9d52296d2d38536c78
Grimaldi Barahona <raul.grimaldi.barahona@intel.com>
2019-04-16 16:14:37 -0600

    Adding third panel on slider

--- src/app/main-slider/main-slider.component.ts
+++ src/app/main-slider/main-slider.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core
 // Angular Animations
 import { animate, state, style, transition, trigger } from '@angular/animations';
 
-type PaneType = 'first' | 'second';
+type PaneType = 'first' | 'second' | 'third';
 
 @Component({
   selector: 'app-main-slider',
@@ -13,7 +13,8 @@ type PaneType = 'first' | 'second';
   animations: [
     trigger('slide', [
       state('first', style({ transform: 'translateX(0)' })),
-      state('second', style({ transform: 'translateX(-50%)' })),
+      state('second', style({ transform: 'translateX(-33.33%)' })),
+      state('third', style({ transform: 'translateX(-66.67%)' })),
       transition('* => *', animate(300))
     ])
   ]
